1adc6926eb215af0669ba81d40e8871d96f1ccef
Adam Wagner <awagner83@gmail.com>
2012-04-07 18:16:30 -0400

    updated Main to use new Compiler module.

--- Main.hs
+++ Main.hs
@@ -1,34 +1,33 @@
 
+import Compiler (compile)
+import Data.Either (lefts)
+
 import Regex
 
-{- Since we don't have a regex compiler yet, here are some prebuild expressions
-   to play with -}
 
-matchChar = Step . LiteralChar
+test = [ matchE   "a"    "a"
+       , matchE   "."    "a"
+       , matchE   ".*"   "aa"
+       , nmatchE  "."    "aa"
+       , nmatchE  "a"    "b"
+       , matchE   "a|b"  "a"
+       , matchE   "a|b"  "b"
+       , nmatchE  "a|b"  "c"
+       , matchE   "a?"   "a"
+       , matchE   "a?"   ""
+       , nmatchE  "a?"   "b"
+       , matchE   "a?b"  "ab"
+       , matchE   "a?b"  "b"
+       , matchE   ".*a"  "ca"
+       , matchE   ".*a"  "abca"
+       ]
 
--- regex: ab+a
-m1 = matchChar 'a' $ repeating (matchChar 'b') $ matchChar 'a' $ MatchEnd
+main = print $ lefts test
 
--- regex: a(bb)+a
-m2 = matchChar 'a' 
-     $ repeating (matchChar 'b' . matchChar 'b') $ matchChar 'a' $ MatchEnd
-
--- regex: bob|fred
-m3 = Split (literal "bob" $ MatchEnd) (literal "fred" $ MatchEnd)
-
-m4 = repeating (Step AnyChar) $ literal ".txt" $ MatchEnd
-
-
-test = [ "a" `matches` (matchChar 'a' $ MatchEnd)
-       , not $ "aa" `matches` (matchChar 'a' $ MatchEnd)
-       , "aba" `matches` m1
-       , "abba" `matches` m2
-       , "bob" `matches` m3
-       , "fred" `matches` m3
-       , not $ "frank" `matches` m3
-       , not $ "freddy" `matches` m3
-       , "foo.txt" `matches` m4
-       , not $ "foo.html" `matches` m4]
-
-main = print test
+match = (flip matches) . compile
+nomatch r s = not (match r s)
+matchE r s | match r s = Right ()
+           | otherwise = Left (r, s)
+nmatchE r s | match r s = Left (r, s)
+            | otherwise = Right ()
 
